8051be9b1e6d49318e1e809dc6b9ac159398ac12
philipptrenz <mail@philipptrenz.de>
2021-06-24 11:34:43 +0200

    fix styling

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="m-auto flex flex-col space-y-4 max-w-sm ">
+  <div class="m-auto flex flex-col space-y-6 max-w-lg bg-white p-6 rounded-2xl">
 
     <h1 class="text-2xl font-bold">{{ $t('privacy.title') }}</h1>
     
