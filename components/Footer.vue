6ba9056e6d1c8c8c98d124280f42bd17ad6f2a10
philipptrenz <mail@philipptrenz.de>
2021-06-24 19:31:16 +0200

    fix some styling

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="w-full flex flex-row justify-center text-xs font-light">
-    <div class="w-full max-w-md flex flex-row justify-between px-8">
+    <div class="w-full max-w-md flex flex-row justify-between px-4 sm:px-8">
       <NuxtLink :to="localePath('/about')" class="">{{ $t('about.title') }}</NuxtLink> | 
       <NuxtLink :to="localePath('/impress')" class="">{{ $t('impress.title') }}</NuxtLink> | 
       <NuxtLink :to="localePath('/privacy')" class="">{{ $t('privacy.title') }}</NuxtLink> |
