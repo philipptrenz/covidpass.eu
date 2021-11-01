689f6d3f42ab067810e66e84d7bd489582340a67
philipptrenz <mail@philipptrenz.de>
2021-11-01 15:31:51 +0100

    hotfix: fix typo

--- pages/index.vue
+++ pages/index.vue
@@ -207,7 +207,7 @@
           <p class="font-light text-xs leading-5" v-html="$t('index.addToWallet.note')"></p>
 
           <div class="flex flex-row flex-wrap ">
-            <img class="pr-2 mt-4" width="160" height="30" src="icons/ios-settings.svg" al="Apple iPhone iOS Settings description">
+            <img class="pr-2 mt-4" width="160" height="30" src="icons/ios-settings.svg" alt="Apple iPhone iOS Privacy Settings description">
 
             <div class="bg-white rounded-lg flex-grow flex flex-row justify-between align-middle px-3 mt-4" style="min-width: 180px; height: 30px;">
               <span class="inline-block text-black text-xs" style="padding: 7.5px 0;">Wallet</span>
