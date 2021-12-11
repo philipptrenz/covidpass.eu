f2484ae30a76a88df2eeec4889c267991e403625
philipptrenz <mail@philipptrenz.de>
2021-12-11 09:52:38 +0100

    fix icon definition

--- nuxt.config.js
+++ nuxt.config.js
@@ -33,8 +33,8 @@ export default {
     ],
     link: [
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
-      { rel: 'icon" type="image/png', sizes: '32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon" type="image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
       { rel: 'manifest', href: '/manifest.json' },
       { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: tailwind.theme.colors.primary },
     ]
