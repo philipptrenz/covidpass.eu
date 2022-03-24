52dad044c0c53af031eae4e3e851797439f62e64
philipptrenz <mail@philipptrenz.de>
2022-03-24 17:22:08 +0100

    update link

--- components/BusinessPass.vue
+++ components/BusinessPass.vue
@@ -16,7 +16,7 @@
           new contacts
         </p>
         <a 
-          href="https://passit.one/" 
+          href="http://passit.one/?mtm_campaign=covidpass.eu" 
           target="_blank" 
           class="mt-2 cursor-pointer w-full h-16 bg-businesspass-button hover:bg-businesspass-button-hover active:bg-businesspass-button-active text-white text-xl lg:text-2xl font-medium rounded-lg px-3 flex flex-row justify-center align-middle space-x-2 transition duration-150 ">
           <span class="self-center">Create yours</span>
